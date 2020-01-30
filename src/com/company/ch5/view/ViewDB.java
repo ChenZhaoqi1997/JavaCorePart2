e7aa7f6ce4c8163bf05881a229ba76affed3893c
Chen Zhaoqi <15101620663@163.com>
2020-01-30 23:54:01 +0800

    first commit

---
+++ src/com/company/ch5/view/ViewDB.java
@@ -0,0 +1,15 @@
+package com.company.ch5.view;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ViewDB {
+    public static void main(String[] args) {
+        EventQueue.invokeLater(() -> {
+            JFrame frame = new ViewDBFrame();
+            frame.setTitle("ViewDB");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setVisible(true);
+        });
+    }
+}
